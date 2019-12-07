5cd0b385d1a7bbcffe9e98b49511bd61a1c357f2
Sam1ola <sam_ola2000.com>
2019-12-07 00:48:54 +0000

    gggggggggggggggggggggggg

--- Project3/src/test/java/StepDefination/MyStepDef.java
+++ Project3/src/test/java/StepDefination/MyStepDef.java
@@ -57,7 +57,7 @@ public class MyStepDef extends Base {
         System.out.println("clicked on buy again  2111");
         System.out.println("clicked on buy again 4  ");
         System.out.println("clicked on buy again 5");
-        System.out.println("clicked on buy again 4  ");
+        System.out.println("clicked on buy again 4cccccccccccccccc  ");
         System.out.println("clickpppppppppppppppppppppppppped on buy again 5");
         System.out.println("clicked on buynnnnnnnnnnnnnnnnnnnnnnnn again 099  ");
         System.out.println("clicked on buy again 999ddddddddddddddddddddddd");
