bae0b6a6b414853e21ec112561a14c4d41fd3d25
Sam1ola <sam_ola2000.com>
2019-12-08 16:13:03 +0000

    new webhook

--- Project3/src/test/java/StepDefination/MyStepDef.java
+++ Project3/src/test/java/StepDefination/MyStepDef.java
@@ -62,6 +62,14 @@ public class MyStepDef extends Base {
         System.out.println("clicked on buynnnnnnnnnnnnnnnnnnnnnnnn again 099  ");
         System.out.println("clicked on buy again 999ddddddddddddddddddddddd");
 
+        System.out.println("clicked on buy again 4cccccccccccccccc  ");
+        System.out.println("clickpppppppppppppppppppppppppped on buy again 5");
+        System.out.println("clicked on buynnnnnnnnnnnnnnnnnnnnnnnn again 099  ");
+        System.out.println("clicked on buy again 999ddddddddddddddddddddddd");
+
+
+
+
     }
 
 
