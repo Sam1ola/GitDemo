e8925e4e01a67008adc30f3d7fc374e8dacc3beb
Sam1ola <sam_ola2000.com>
2019-12-08 18:23:28 +0000

    bcbxzcvb

--- Project3/src/test/java/StepDefination/MyStepDef.java
+++ Project3/src/test/java/StepDefination/MyStepDef.java
@@ -123,7 +123,7 @@ public class MyStepDef extends Base {
        // driver= Base.getDriver();
 
         System.out.println("clicked on buy again 3  ");
-        System.out.println("clicked on buy again 4fffffffffffffff ");
+        System.out.println("clicked on buy again 4fffffffffffffffffffffffffffffffff ");
         System.out.println("clickeddscgnfhzddsddddddddddddddddgfhatjhgfs on buy again 3  ");
         System.out.println("clicked on buy again 4 ");
 
